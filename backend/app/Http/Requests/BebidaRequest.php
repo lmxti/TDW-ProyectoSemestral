7d69eaf47bcb1560852fe141d52d036f5a9dffcf
DJVegaG <domingo.vega1901@alumnos.ubiobio.cl>
2023-07-05 18:22:36 -0400

    UPDATE RULES BEBIDAS

--- backend/app/Http/Requests/BebidaRequest.php
+++ backend/app/Http/Requests/BebidaRequest.php
@@ -24,8 +24,43 @@ class BebidaRequest extends FormRequest
      */
     public function rules(): array
     {
+        if ($this->isMethod('post')){
+            return [
+                'nombre' => 'required|string',
+                'sabor' => 'required|alpha',
+                'tamano' => 'required|string',
+            ];
+        }elseif ($this->isMethod('put')){
+            return [
+                'id' => 'required|integer',
+                'nombre' => 'required|string',
+                'sabor' => 'required|string',
+                'tamano' => 'required|string',
+            ];
+        }elseif ($this->isMethod('delete')){
+            return [
+                'id' => 'required|integer',
+            ];
+        }else{
+            return [];
+        }
+    }
+
+    public function messages(){
         return [
-            //
+            'nombre.required' => 'El nombre es requerido',
+            'sabor.required' => 'El sabor es requerido',
+            'tamano.required' => 'El tamaño es requerido',
+            'id.required' => 'El id es requerido',
+            'id.integer' => 'El id debe ser un entero',
+            'sabor.alpha' => 'El sabor debe ser alfabético',
         ];
+    }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+            "message" => "Error de validación",
+            "errors" => $validator->errors()
+        ], Response::HTTP_BAD_REQUEST));
     }
 }
